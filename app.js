3507863a969c1c5ea28cc57bf1107b6360b86bf1
Kyle Devine <devinenoise@gmail.com>
2020-01-13 23:21:06 -0800

    html semantics fixed and styled poor results score

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ import isYes from '/src/isYes.js';
 
 const quizButton = document.getElementById('quiz-button');
 const total = document.getElementById('total');
-
+const lowTotal = document.getElementById('lowtotal');
 
 quizButton.addEventListener('click', () => {
     const name = prompt("What\'s your name?");
-    const confirmation = confirm("Are you sure you want to take the quiz?");
+    const confirmation = confirm(`Hello, ${name}, are you ready to take the quiz?`);
     if (confirmation === false) return;
     const answer1 = prompt("Did I live in Montana?");
-    const answer2 = prompt("Is my kid named Roger?");
+    const answer2 = prompt("Is one of my kids named Roger?");
     const answer3 = prompt("Do I like music?");
-    const farewell = confirm("The quiz is complete.  See your results!")
+    const farewell = confirm(`Ok, ${name}, the quiz is complete.  See your results!`)
     let count = 0;
     if (isYes(answer1)) count += 1;
     if (!isYes(answer2)) count += 1;
     if (isYes(answer3)) count += 1;
 
-    total.textContent = `${name}, you got ${count} answers correct!`;
+    const answerTotals = 3;
+
+    if (count >= 2) {
+        total.textContent = `Great job ${name}, you got ${count}/${answerTotals} answers correct!`;
+    } else {
+        lowTotal.textContent = `Try again ${name}, you got ${count}/${answerTotals} answers correct!`;
+    }
 });
